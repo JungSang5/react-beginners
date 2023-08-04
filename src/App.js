88e68bd8103aa2b0215746dd59173aa3c2d64e87
JungSang5 <mosea1541@naver.com>
2023-08-04 14:50:10 +0900

    230804 work 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log("I run all the time");
+  useEffect(() => {
+    console.log("i run only once");
+  }, []);
+  useEffect(() => {
+    if (keyword !== "" & keyword.length > 5) {
+      console.log("SEARCH FOR", keyword);
+    }
+  }, [keyword]);
   return (
     <div>
-      <h1 className={styles.title}>Welcome Back!@#$%</h1>
-      <Button text={"Continue"} />
-    </div >
-  );
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click Me</button>
+    </div>
+  )
 }
-
 export default App;
