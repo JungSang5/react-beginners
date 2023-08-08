772496f4ca2f9ac126d2faf64cd4e7aef95f78f7
JungSang5 <mosea1541@naver.com>
2023-08-08 13:26:41 +0900

    230808-work1-end

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const [toDo, setTodo] = useState("");
-  const onCahnge = (event) => setTodo(event.target.value);
-  console.log(toDo);
-  return <div>
-    <form>
-      <input onChange={onCahnge} value={toDo} type="text" placeholder="Write your to do..."></input>
-      <button>Add To Do</button>
-    </form>
-  </div>
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onCahnge = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+  console.log(toDos);
+  return (
+    <div>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input onChange={onCahnge} value={toDo} type="text" placeholder="Write your to do..."></input>
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => <li key={index}>{item}</li>)}
+      </ul>
+    </div>
+  )
 }
 export default App;
